1830a346dc3b05684f4b75e0ed98f9e3fba10eff
DJ1TJOO <t.ferb1@gmail.com>
2018-11-08 12:49:12 +0100

    GameState Added

---
+++ src/GameState/GameStates/World1.java
@@ -0,0 +1,37 @@
+package GameState.GameStates;
+
+import GameState.GameState;
+import GameState.GameStateManager;
+
+import java.awt.*;
+
+public class World1 extends GameState {
+    public World1(GameStateManager gsm) {
+        this.gsm = gsm;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void render(Graphics2D g) {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void keyPressed(int k) {
+
+    }
+
+    @Override
+    public void keyReleased(int k) {
+
+    }
+}
